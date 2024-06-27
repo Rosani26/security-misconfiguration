f98449ea365759377aaa450ceb6ee613ad72be7c
Rosani26 <94964550+Rosani26@users.noreply.github.com>
2024-06-27 14:05:44 +0200

    Files via upload commited
    from existing project

---
+++ Security-misconfiguration/src/main/java/org/example/securitymisconfiguration/SecurityMisconfigurationApplication.java
@@ -0,0 +1,13 @@
+package org.example.securitymisconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMisconfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityMisconfigurationApplication.class, args);
+    }
+
+}
---
+++ Security-misconfiguration/src/test/java/org/example/securitymisconfiguration/SecurityMisconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securitymisconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMisconfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
